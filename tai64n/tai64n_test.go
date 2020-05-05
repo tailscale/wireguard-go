4e5fea324f88a8071fa6830429f7e3e113e8b25d
Dmytro Shynkevych <dmytro@tailscale.com>
2020-05-05 09:21:10 -0400

    tai64n: test using a deterministic API
    Signed-off-by: Dmytro Shynkevych <dmytro@tailscale.com>

--- tai64n/tai64n_test.go
+++ tai64n/tai64n_test.go
@@ -10,21 +10,28 @@ import (
 	"time"
 )
 
-/* Testing the essential property of the timestamp
- * as used by WireGuard.
- */
+// Test that timestamps are monotonic as required by Wireguard and that
+// nanosecond-level information is whitened to prevent side channel attacks.
 func TestMonotonic(t *testing.T) {
-	old := Now()
-	for i := 0; i < 50; i++ {
-		next := Now()
-		if next.After(old) {
-			t.Error("Whitening insufficient")
-		}
-		time.Sleep(time.Duration(whitenerMask)/time.Nanosecond + 1)
-		next = Now()
-		if !next.After(old) {
-			t.Error("Not monotonically increasing on whitened nano-second scale")
-		}
-		old = next
+	// Whitening should reduce timestamp granularity
+	// to more than 10 but fewer than 20 milliseconds.
+	tests := []struct {
+		name      string
+		t1, t2    time.Time
+		wantAfter bool
+	}{
+		{"same_microsecond", time.Unix(0, 123123456), time.Unix(0, 123123654), false},
+		{"10_milliseconds", time.Unix(0, 123123456), time.Unix(0, 133456123), false},
+		{"20_milliseconds", time.Unix(0, 123123456), time.Unix(0, 143321654), true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts1, ts2 := stamp(tt.t1), stamp(tt.t2)
+			got := ts2.After(ts1)
+			if got != tt.wantAfter {
+				t.Errorf("after = %v; want %v", got, tt.wantAfter)
+			}
+		})
 	}
 }
