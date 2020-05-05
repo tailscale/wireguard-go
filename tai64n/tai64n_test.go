cbd1405bd03bd406656fef25aa0ac8a04ff55317
Dmytro Shynkevych <dmytro@tailscale.com>
2020-05-05 09:21:10 -0400

    tai64n: test using a deterministic API
    Signed-off-by: Dmytro Shynkevych <dmytro@tailscale.com>

--- tai64n/tai64n_test.go
+++ tai64n/tai64n_test.go
@@ -10,21 +10,29 @@ import (
 	"time"
 )
 
-/* Testing the essential property of the timestamp
- * as used by WireGuard.
- */
+// Test that timestamps are monotonic as required by Wireguard and that
+// nanosecond-level information is whitened to prevent side channel attacks.
 func TestMonotonic(t *testing.T) {
-	old := Now()
-	for i := 0; i < 50; i++ {
-		next := Now()
-		if next.After(old) {
-			t.Error("Whitening insufficient")
+	ts := []Timestamp{
+		stamp(time.Unix(0, 123123456)),
+		stamp(time.Unix(0, 123123654)), // within the same microsecond
+		stamp(time.Unix(0, 133456123)), // in ~10 milliseconds
+		stamp(time.Unix(0, 143321654)), // in ~20 milliseconds
+	}
+
+	// Timestamps in Wireguard must grow monotonically with passage of time.
+	for i := 0; i < len(ts)-1; i++ {
+		if ts[i].After(ts[i+1]) {
+			t.Error("Timestamps not monotonic")
 		}
-		time.Sleep(time.Duration(whitenerMask)/time.Nanosecond + 1)
-		next = Now()
-		if !next.After(old) {
-			t.Error("Not monotonically increasing on whitened nano-second scale")
-		}
-		old = next
+	}
+
+	// Whitening should reduce timestamp granularity
+	// to more than 10 but fewer than 20 milliseconds.
+	if ts[1].After(ts[0]) || ts[2].After(ts[0]) {
+		t.Error("Whitening insufficient")
+	}
+	if !ts[3].After(ts[0]) {
+		t.Error("Not monotonically increasing on whitened nano-second scale")
 	}
 }
