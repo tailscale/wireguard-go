4fa12472966737b7566d768dffb1a16e00316e01
Jordan Whited <jordan@tailscale.com>
2022-12-19 11:08:06 -0800

    tun: replace use of binary package with copy() in virtioNetHdr encoding (#3)
    This is a cherry-pick of 798bed0b5d9283a45d9f3e78da7f5acd7477b7a6.
    
    Signed-off-by: Jordan Whited <jordan@tailscale.com>

--- endian/little.go
+++
@@ -1,8 +0,0 @@
-//go:build 386 || amd64 || arm || arm64 || mips64le || mipsle || ppc64le || riscv64 || wasm || loong64
-
-package endian
-
-import "encoding/binary"
-
-// Native is the platform's native byte order.
-var Native = binary.LittleEndian
